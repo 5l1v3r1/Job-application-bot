4a4221fd390dc4a42f28e057ff6b26d24bdc82a1
JasonNgassa <54824966+JasonNgassa@users.noreply.github.com>
2021-01-08 00:08:23 -0500

    Add files via upload
    Contains all tutor's info

---
+++ Main/tutor_secrets.py
@@ -0,0 +1,29 @@
+# Enter your credentials here
+
+#ENTER USER NAME HERE
+user_name = "michael.luma"
+
+#ENTER PASSWORD HERE
+password = "Y2xUPE84zgWJ%RR"
+
+#ENTER THE TUTOR'S STANDARD HOURLY RATE
+rate = 35 
+
+#ENTER THE TUTOR'S COURSES YOU WANT TO TUTOR
+coursesTaught = ["Python", "Calculus", "French"]
+
+#ENTER KEYWORDS TO TURN DOWN JOB
+dealBreakers = ["machine learning", "matplotlib", "django", "data science", "pandas"]
+
+#ENTER THE NUMBER OF JOBS TO APPLY
+jobstoApply = 3
+
+pitches = { "Python": "Hello client_name, how good am I at python programming? Well, this job application was actually made by one of the bots I made using python. " + 
+						"On a more serious note, I have been a tutor for more than 3 years now and I assisted have multiple students reach their academic goals " + 
+						"such as this bot for example!",
+			"Calculus": "Hello client_name, I have been successfully tutoring Math classes ranging from Pre-algebra to Multivariable Calculus (Calculus 2) for the past 5 years "+ 
+						"and have seen some flourishing results in my students as they went from being inept in a Math class to successfully grasping essential concepts and even averaging an A in the class!",
+			"French": "Hello client_name, I am a native french speaker and I have been tutoring French for the past 4 years "+
+						"with some very fruitful results, helping them gaining enough confidence to Ace their class and add French to their resumes!"
+
+			}
